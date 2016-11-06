fbd84004e05e89ecaffe8f5e8be7e1392218c35d
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-06 13:38:22 -0500

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+
+var totalBatteries = batteryBatches.reduce(function(a,b){
+  return a + b;
+})
+
+
+
+// wordCount is the 'accumulator', inital value being '{}'
+// sentence is the curent iteration through the monologueLines array
+
+var wordCountMap = monologueLines.reduce(function(wordCount, sentence){
+  var length = sentence.split(' ').length.toString() // find the length of the current sentence
+  if (!Object.keys(wordCount).includes(length)){ // if that length is not a current key in wordCount
+      wordCount[length] = 1 // create the key in WordCount and set its value to 1
+    } else {
+      wordCount[length] += 1 // if that length is already a key, then increment its value by 1
+    }
+    return wordCount; // return the whole accumulator after iterating through all of the sentences
+  }, {}); // this sets the initial value of the accumulator to an object
+
